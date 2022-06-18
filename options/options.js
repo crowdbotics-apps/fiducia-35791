a3d432480c2495ed715bedc185393efd1500d2b2
Crowdbotics <team@crowdbotics.com>
2022-06-18 22:41:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "fiducia_35791",
+  url: "https://fiducia_35791.botics.co",
+  api: "https://fiducia_35791.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
